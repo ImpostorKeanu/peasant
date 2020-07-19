a1bf7e8649eea1a923256c96a3289c89ab1b84f0
Justin Angel <justinangel86@gmail.com>
2020-07-19 13:55:30 -0400

    not working....yet

--- Peasant/harvest.py
+++ Peasant/harvest.py
@@ -3,11 +3,63 @@ from Peasant.suffix_printer import *
 from Peasant.extractors import *
 from Peasant.generic import *
 
+class Company:
+
+    def __init__(self,company_name,step_size=10):
+
+        # track the company_name
+        self.company_name=company_name
+        self.cn=self.company_name
+
+        # maintain step size for each profile request
+        self.step_size=step_size
+        self.ss=self.step_size
+
+        # maintain previous offset call for next call
+        self.last_offset=0
+
+        # keep track of profiles for each company name
+        self.profiles=[]
+
+        self.company_id = None
+        self.profile_count = None
+
+        # ========================================
+        # GET THE company_id FROM THE company_name
+        # ========================================
+
+        try:
+            cid = company_id = session.getCompanyId(company_name)
+            esprint(f'Company Identifier for {company_name}: {cid}')
+        except SessionException:
+            esprint(f'Failed to get company identifier for {company_name} ' \
+                    'Continuing to next company')
+
+        # ========================
+        # EXTRACT INITIAL PROFILES
+        # ========================
+
+        esprint('Getting initial profiles')
+        resp = session.getContactSearchResults(cid,0,10)
+        count, profiles = extractInfo(resp.json(),company_name,cid)
+        esprint(f'Available profiles: {count}')
+
+    def step(self):
+
+        ceiling=self.last_offset+self.step_size
+        out=(self.last_offset,ceiling,)
+        self.last_offset=ceiling
+        return out
+
 def harvest_contacts(args,session,main_profiles=[]):
 
     # ============================
     # BEGIN EXTRACTING INFORMATION
     # ============================
+
+    # Get initial profiles for each company_name value
+    # Initialize a member of the OFFSETS variable for that company_name
+    #    - OFFSETS["company_name"]=(last_min,last_max)
    
     for company_name in args.company_names:
     
